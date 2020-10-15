3c2a0511dadc24974efc8ad04916c2909c28af67
David Morales <davmorales@ucdavis.edu>
2020-10-14 22:35:01 -0700

    Added get mappings to controller

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.cognixia.jump.controller;
 
+import com.cognixia.jump.exceptions.ResourceNotFoundException;
 import com.cognixia.jump.model.User;
 import com.cognixia.jump.repo.UserRepo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Controller for Users
@@ -35,4 +38,21 @@ public class UserController {
     public List<User> getUsers() {
         return service.findAll();
     }
+
+    @GetMapping("/user/{id}")
+    @ApiOperation(value = "",
+    notes = "Retrieve a user pertaining to an id. \n" +
+            "Usage: provide an id to look up a user in the database\n" +
+            "Author(s): David Morales\n" +
+            "Exception(s): ResourceNotFoundException is thrown when the id does not match an exisiting user in the database",
+    response = User.class, produces = "application/json")
+    public User getUserById(@PathVariable Long id) throws ResourceNotFoundException {
+        Optional<User> found = service.findById(id);
+        if(!found.isPresent()) {
+            throw new ResourceNotFoundException("User with id= " + id + " is not found");
+        }
+
+        return found.get();
+    }
+
 }
